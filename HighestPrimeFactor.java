548b6dc39cdda0fcba276edfd8dd8a1ff89debfa
bhupinder414 <bhupinderpal414@gmail.com>
2019-12-06 18:57:31 +0530

    highest prime factor

---
+++ HighestPrimeFactor.java
@@ -0,0 +1,28 @@
+package git;
+import java.util.Scanner;
+
+public class HighestPrimeFactor {
+
+	public static void main(String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter number");
+		int num=in.nextInt();
+		int flag=0;
+                for(int i=1;i<num;i++)
+                {
+                  flag=0;
+                  int n=num/i;
+                  for(int j=2;j<=n/2;j++)
+                  {
+        	  if(n%j==0)
+        	  flag=1;
+                  }
+                  if(flag==0 && num%n==0)
+                  {
+        	  System.out.println(n);
+        	  break;
+                  }
+                }
+	}
+}
